64ce5e465ec1b57a638e49a3cdfb1c4c8fe8d1a5
Seth Silesky <seth.silesky@gmail.com>
2017-10-29 13:08:56 -0500

    add inputs etc, style

--- src/App.js
+++ src/App.js
@@ -2,31 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import initialEvalList from './data';
 
-// const escapeQuotes = (str) => (`${str}`).replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
-
 class App extends Component {
   state = {
+    evalsLive: ["'123'", '123'],
     evalsList: initialEvalList,
-    input: '',
   };
 
   handleChange = ({ target: { value: textInput } }) => {
-    this.setState(() => ({ input: textInput }));
-  };
-  handleSubmit = (e) => {
-    e.preventDefault(); // so page does not reload, but we want the enter button to work.
-    this.setState(({ evalsList, input }) => ({
-      evalsList: [[`'${input}'`, input], ...evalsList],
-    }));
+    this.setState(() => ({ evalsLive: [`'${textInput}'`, textInput] }));
   };
   render() {
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Submit</button>
-        </form>
-        <Table evalsList={this.state.evalsList} />
+        <h1>
+          Ultimate Javascript Number Parsing Functions & Operator Comparison
+          Table
+        </h1>
+        <div className="input-container">
+          <input
+            className="input"
+            placeholder="Type something here."
+            onChange={this.handleChange}
+            type="text"
+          />
+        </div>
+        <Table
+          evalsLive={this.state.evalsLive}
+          evalsList={this.state.evalsList}
+        />
       </div>
     );
   }
@@ -41,7 +44,7 @@ const objToString = obj =>
       return acc;
     }, {});
 
-const Table = ({ evalsList }) => {
+const Table = ({ evalsList, evalsLive }) => {
   const createEvaluations = nums => {
     return nums.map(([label, value]) => {
       return objToString({
@@ -55,44 +58,68 @@ const Table = ({ evalsList }) => {
       });
     });
   };
+  const Head = () => (
+    <thead>
+      <tr>
+        <th>X</th>
+        <th>parseInt(x)</th>
+        <th>parseFloat(x)</th>
+        <th>Number(x)</th>
+        <th>+x</th>
+        <th>--x</th>
+        <th>x>>>0</th>
+      </tr>
+    </thead>
+  );
+  const [elive] = createEvaluations([evalsLive]);
   const evaluations = createEvaluations(evalsList);
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>X</th>
-          <th>parseInt(x)</th>
-          <th>parseFloat(x)</th>
-          <th>Number(x)</th>
-          <th>+x</th>
-          <th>--x</th>
-          <th>x>>>0</th>
-        </tr>
-      </thead>
-      <tbody>
-        {evaluations.map(({
-            evaluee,
-            _parseInt,
-            _parseFloat,
-            _Number,
-            _unary,
-            _unaryMinus,
-            _bitwise,
-          }) => {
-            return (
-              <tr key={`${evaluee}${Math.random()}`}>
-                <td>{evaluee}</td>
-                <td>{_parseInt}</td>
-                <td>{_parseFloat}</td>
-                <td>{_Number}</td>
-                <td>{_unary}</td>
-                <td>{_unaryMinus}</td>
-                <td>{_bitwise}</td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </table>
+    <div>
+      <table className="table">
+        <Head />
+        <tbody>
+          <tr key={`${elive.evaluee}${Math.random()}`}>
+            <td>{elive.evaluee}</td>
+            <td>{elive._parseInt}</td>
+            <td>{elive._parseFloat}</td>
+            <td>{elive._Number}</td>
+            <td>{elive._unary}</td>
+            <td>{elive._unaryMinus}</td>
+            <td>{elive._bitwise}</td>
+          </tr>
+        </tbody>
+      </table>
+      <hr />
+      <table className="table">
+        <Head />
+        <tbody>
+          {evaluations.map(
+            ({
+              evaluee,
+              _parseInt,
+              _parseFloat,
+              _Number,
+              _unary,
+              _unaryMinus,
+              _bitwise,
+            }) => {
+              return (
+                <tr key={`${evaluee}${Math.random()}`}>
+                  <td>{evaluee}</td>
+                  <td>{_parseInt}</td>
+                  <td>{_parseFloat}</td>
+                  <td>{_Number}</td>
+                  <td>{_unary}</td>
+                  <td>{_unaryMinus}</td>
+                  <td>{_bitwise}</td>
+                </tr>
+              );
+            },
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
